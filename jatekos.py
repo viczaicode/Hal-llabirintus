2a98974b74dc0d381aef0bf350a9f0d650e13595
viczaicode <viczaimilan2003@gmail.com>
2024-11-27 13:07:21 +0100

    2 kockadobas osszege megcsinalva

--- jatekos.py
+++ jatekos.py
@@ -2,22 +2,31 @@ import random
 
 class Jatekos():
 
-    def __init__(self, nev:str="Játékos"):
+    def __init__(self, nev:str="Játékos", ugyesseg:int=0, eletero:int=0, szerencse:int=0):
         
         self.nev = nev
+        self.ugyesseg = ugyesseg
+        self.eletero = eletero
+        self.szerencse = szerencse
         self.set_ugyesseg()
-        self.eletero()
-        self.szerencse()
+        self.set_eletero()
+        self.set_szerencse()
         
     def set_ugyesseg(self):
         self.ugyesseg = random.randint(1,6) + 6
        
     
-    def eletero(self):
+    def set_eletero(self):
         self.eletero = random.randint(1,6) + 12
     
-    def szerencse(self):
+    def set_szerencse(self):
         self.szerencse = random.randint(1,6) + 6
     
+    def ketto_kockadobas_osszegadva(self):
+        dobas1:int = self.set_ugyesseg()
+        dobas2:int = self.set_ugyesseg()
+
+        return dobas1 + dobas2
+        
     def __str__(self):
         return f"➝ Ügyességed: {self.ugyesseg} ✦\n ➝ Életerőd: {self.eletero} ❤️\n ➝ Szerencséd: {self.szerencse} 🍀"
