e00df7ef5f84b51fa6ab0a13034c17b9545d21bd
Torderius <tordailevi@gmail.com>
2024-11-28 09:30:01 +0100

    oldalas oldalak

--- jatekmenet.py
+++ jatekmenet.py
@@ -1,31 +1,41 @@
 import oldalak
+from jatekos import Jatekos
 
-def alagut_kaland():
-    valasztas = input("A dobozt kinyitod (lapozz 270-re) vagy továbbmész (lapozz 66-ra)? ➝")
+
+def labirintus():
+    valasztas = input("A dobozt kinyitod (lapozz 270-re) vagy továbbmész (lapozz 66-ra)? ➝ ")
     if valasztas == "270":
         oldalak.Oldal270()
+        '''Arany hozzáadása'''
         print("Begyűjtöd az aranyat!\n")
         oldalak.Oldal66()
-        valasztas = input("Nyugat (lapozz 293-ra) vagy Kelet (lapozz 56-ra)? ➝")
+        valasztas = input("Nyugat (lapozz 293-ra) vagy Kelet (lapozz 56-ra)? ➝ ")
         if (valasztas == "56"):
             oldalak.Oldal56()
-            valasztas = input("Átmászol (lapozz 373-ra) vagy kettévágod (lapozz 215-re)? ➝")
+            valasztas = input("Átmászol (lapozz 373-ra) vagy kettévágod (lapozz 215-re)? ➝ ")
         elif (valasztas == "293"):
             oldalak.Oldal293()
+            valasztas = input("Nyugat (lapozz 387-re) vagy Észak (lapozz 137-ra)? ➝ ")
+        if (valasztas == "137"):
+            oldalak.Oldal137()
+        elif (valasztas == "387"):
+            oldalak.Oldal387()
+            '''Barlangi Ember'''
     elif valasztas == "66":
         oldalak.Oldal66()
-        valasztas = input("Nyugat (lapozz 293-ra) vagy Kelet (lapozz 56-ra)? ➝")
+        valasztas = input("Nyugat (lapozz 293-ra) vagy Kelet (lapozz 56-ra)? ➝ ")
         if valasztas == "293":
             oldalak.Oldal293()
-            valasztas = input("Ha továbbmész nyugat felé a lábnyomokat követve, lapozz a 137-re. Ha inkább észak felé mész a harmadik pár lábnyom után, lapozz a 387-re. ➝")
+            valasztas = input("Ha továbbmész nyugat felé a lábnyomokat követve, lapozz a 137-re. Ha inkább észak felé mész a harmadik pár lábnyom után, lapozz a 387-re. ➝ ")
             if valasztas == "137":
                 oldalak.Oldal137()  
             elif valasztas == "387":
                 oldalak.Oldal387()
         elif valasztas == "56":
             oldalak.Oldal56()
-            valasztas = input("Átmászol (lapozz 373-ra) vagy kettévágod (lapozz 215-re)? ➝")
+            valasztas = input("Átmászol (lapozz 373-ra) vagy kettévágod (lapozz 215-re)? ➝ ")
             if valasztas == "373":
                 oldalak.Oldal373()
             elif valasztas == "215":
                 oldalak.Oldal215()
+                '''-2hp'''
--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import jatekmenet
 
-jatekmenet.alagut_kaland()
+jatekmenet.labirintus()
